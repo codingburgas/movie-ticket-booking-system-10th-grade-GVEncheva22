5fccecaf1dd847037b72f1ac99c6da423e1cd95b
GVEncheva22 <GVEncheva22@codingburgas.bg>
2025-04-29 15:36:09 +0300

    Implement start Menu

--- MovieTicketBookingSystem/MovieTicketBookingSystem/mainMenu.cpp
+++ MovieTicketBookingSystem/MovieTicketBookingSystem/mainMenu.cpp
@@ -58,3 +58,96 @@ public:
 		}
 	}
 };
+
+void startMenu()
+{
+	while (true)
+	{
+		int mainCheck = 0;
+		int newCheck = 0;
+		system("cls");
+		//aboutPage();
+		cin >> newCheck;
+		system("cls");
+		if (newCheck == 1)
+		{
+			cout << " Welcome to..." << endl;
+			cout << " 1. Administrator" << endl;
+			cout << " 2. Customer" << endl;
+			cin >> mainCheck;
+			if (mainCheck == 1)
+			{
+				char userName[100];
+				char pass[100];
+				cout << "Enter username: ";
+				cin >> userName;
+				cout << "Enter password: ";
+				cin >> pass;
+				char user[100];
+				char userPass[100];
+				int nCheck = 0;
+				int aCheck = 0;
+				ifstream userData("admins.txt");
+				{
+					if (!userData)
+					{
+						cout << "Cannot open file!";
+					}
+					while (!(userData.eof()))
+					{
+						userData >> user >> userPass;
+						if (compare(user, userName))
+						{
+							nCheck = 1;
+						}
+						if (compare(userPass, pass))
+						{
+							aCheck = 1;
+						}
+					}
+					if (nCheck == 1 and aCheck == 1)
+					{
+						system("cls");
+						Menu am;
+						am.displayMenu();
+						system("cls");
+					}
+				}
+			else
+			{
+				cout << "Permission denied!\n";
+				cout << "Press any key to go back: ";
+				system("cls");
+			}
+			}
+			else if (newCheck == 3)
+				exit(0);
+		}
+		else
+		{
+			int rCheck = 0;
+			int userSelect = 0;
+			cout << "Enter personal details" << endl;
+			Customer c2;
+			c2.getData();
+			getch();
+			system("cls");
+			cout << " 1. Display movies" << endl;
+			cout << " 2. Select movie" << endl;
+			cout << " 3. Exit" << endl;
+			cin >> userSelect;
+			Movie m2;
+			Receipt r2;
+			Hall h2;
+
+			switch (userSelect)
+			{
+			case 1:
+				system("cls");
+				m2.displayMovie();
+				cout << "Press 1 to select movie";
+				cin >> rCheck;
+				if (rCheck == 1)
+				{
+					goto select;
+				}
