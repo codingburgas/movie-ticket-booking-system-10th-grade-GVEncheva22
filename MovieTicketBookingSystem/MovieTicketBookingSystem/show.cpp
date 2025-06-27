c323d2ce4498df3c5d42b3f79bba50e478a6f6dd
GVEncheva22 <GVEncheva22@codingburgas.bg>
2025-06-27 19:00:43 +0300

    Add Show class with seat booking and info display

--- MovieTicketBookingSystem/MovieTicketBookingSystem/show.cpp
+++ MovieTicketBookingSystem/MovieTicketBookingSystem/show.cpp
@@ -1,1 +1,19 @@
-#include "show.h"
+#include "Show.h"
+#include <iostream>
+using namespace std;
+
+Show::Show(string title, string time, Hall* hall) {
+    this->movieTitle = title;
+    this->showTime = time;
+    this->hall = hall;
+}
+
+void Show::displayShowInfo() const {
+    cout << "Movie: " << movieTitle << endl;
+    cout << "Time: " << showTime << endl;
+    cout << "Hall Number: " << hall->hallNumber << endl;
+}
+
+bool Show::bookSeat(int seatNumber) {
+    return hall->bookSeat(seatNumber);
+}
--- MovieTicketBookingSystem/MovieTicketBookingSystem/show.h
+++ MovieTicketBookingSystem/MovieTicketBookingSystem/show.h
@@ -1,5 +1,17 @@
 #pragma once
-class show
-{
-};
+#include "Hall.h"
+#include <string>
+using namespace std;
 
+class Show {
+private:
+    string movieTitle;
+    string showTime;
+    Hall* hall; 
+
+public:
+    Show(string title, string time, Hall* hall);
+
+    void displayShowInfo() const;
+    bool bookSeat(int seatNumber);
+};
